b5abdc908a33ad57c90dae09b0465f885417df26
Josicleison Elves <engenhariaeletricaquinto@gmail.com>
2022-12-14 16:24:17 -0300

    created Create functions that creates UI Elements

--- SwiftStudy/AppUtils/Create/Create.swift
+++ SwiftStudy/AppUtils/Create/Create.swift
@@ -5,13 +5,6 @@
 //  Created by Josicleison Elves on 14/12/22.
 //
 
-import UIKit
+import Foundation
 
-struct Create {
-    static func label(_ text: String? = nil) -> UILabel {
-        let label = UILabel()
-        label.text = text
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        return label
-    }
-}
+struct Create {}
